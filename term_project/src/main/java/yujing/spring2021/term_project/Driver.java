fe63a7d8e585367264d02079a1eab94a98dfc0b2
YujinGao96 <gao.yujing.csu@gmail.com>
2021-04-05 12:31:20 -0400

    first commit

---
+++ term_project/src/main/java/yujing/spring2021/term_project/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yujing.spring2021.term_project;
+
+/**
+ *
+ * @author yujinggao
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Starting here");
+    }
+}
